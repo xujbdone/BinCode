5e9583f14e56901a384e04a2d9cbb573e71c747c
XuJinBin <xujbdone@outlook.com>
2024-08-01 16:48:20 +0800

    reids

---
+++ 06-Redis/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _06_Redis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //连接
+            var constr = $"127.0.0.1:6379,ssl=false,writeBuffer=10240,prefix=fz";
+            var csredis = new CSRedis.CSRedisClient(constr);
+            RedisHelper.Initialization(csredis);
+            RedisHelper.Set("name", "xujinbin");
+            var name = RedisHelper.Get("name");
+
+            Console.ReadKey();
+        }
+    }
+}
